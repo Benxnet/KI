6203d83e66112db54033c9fbfeae80634e73f6aa
Benedikt Andreas Ulrich Nettebrock <bn083824@fh-muenster.de>
2022-05-17 18:18:32 +0200

    asdad

--- K3/A-Star.py
+++ K3/A-Star.py
@@ -1,5 +1,6 @@
 import json
 import math
+import time
 
 
 class AStar:
@@ -39,23 +40,29 @@ class AStar:
         self.ziel = ziel
         self.distance[start] = 0
         self.frontier.append(start)
+        explored = list()
 
-        while len(self.frontier) != 0:  #if EMPTY?( _frontier ) then
-            v = self.getLowestCost()    #_node ← POP ( _frontier ) /* chooses the lowest-cost node in frontier */
-            if v == self.ziel:          #if _problem.GOAL-TEST ( _node.STATE ) then
-                return self.solution()  #return SOLUTION ( _node )
-            self.frontier.remove(v)     #add _node.STATE to _explored
-            for knoten in self.adjazenzliste[v].keys(): #for each _action in _problem.ACTIONS ( _node.STATE ) do
+        while len(self.frontier) != 0:  # if EMPTY?( _frontier ) then
+            v = self.getLowestCost()  # _node ← POP ( _frontier ) /* chooses the lowest-cost node in frontier */
+            print("Explore:" + v)
+            if v == self.ziel:  # if _problem.GOAL-TEST ( _node.STATE ) then
+                return self.solution()  # return SOLUTION ( _node )
+            self.frontier.remove(v)  # add _node.STATE to _explored
+            explored.append(v)
+            for knoten in self.adjazenzliste[v].keys():  # for each _action in _problem.ACTIONS ( _node.STATE ) do
+
                 if self.distance[knoten] == math.inf or self.distance[knoten] > self.distance[v] + \
-                        self.adjazenzliste[v][knoten]:  #if _child.STATE is not in _explored or _frontier then bzw günstigsten
-                    self.distance[knoten] = self.distance[v] + self.adjazenzliste[v][knoten]    #kosten
-                    self.frontier.append(knoten)    #_frontier ← INSERT ( _child, _frontier )
+                        self.adjazenzliste[v][
+                            knoten] and knoten not in explored:  # if _child.STATE is not in _explored or _frontier then bzw günstigsten
+
+                    self.distance[knoten] = self.distance[v] + self.adjazenzliste[v][knoten]  # kosten
+
+                    self.frontier.append(knoten)  # _frontier ← INSERT ( _child, _frontier )
 
     def getLowestCost(self):
         cheap = self.frontier[0]
         for knoten in self.frontier:
             if self.distance[knoten] + self.heuristic(knoten) < self.distance[cheap]:
-                print(knoten)
                 cheap = knoten
         return cheap
 
@@ -78,10 +85,15 @@ class AStar:
     def heuristic(self, knoten):
         delta_x = self.koordinaten[knoten]["x"] - self.koordinaten[self.ziel]["x"]  # abstand x
         delta_y = self.koordinaten[knoten]["y"] - self.koordinaten[self.ziel]["y"]  # abstand y
-        return math.sqrt(math.pow(delta_x, 2) + math.pow(delta_y, 2)) * 100  # Hypotenuse oder luftlinie
-        #return 0
+        return math.sqrt(math.pow(delta_x, 2) + math.pow(delta_y, 2)) * 200  # Hypotenuse oder luftlinie
 
 
 if __name__ == "__main__":
     search = AStar()
-    print(search.aStarAlg("c", "g"))
+    start = ""
+    ziel = ""
+    while start not in {"a", "b", "c", "d", "e", "f2", "g", "h", "i", "j"}:
+        start = input("Start: ")
+    while ziel not in {"a", "b", "c", "d", "e", "f2", "g", "h", "i", "j"}:
+        ziel = input("Ziel: ")
+    print(search.aStarAlg(start, ziel))
